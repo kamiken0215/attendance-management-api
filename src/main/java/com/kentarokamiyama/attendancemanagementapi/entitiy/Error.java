1f1ca126c5ce57ff339e7f6989cb3cb1133501dc
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-08-23 14:53:30 +0900

    user controller

---
+++ src/main/java/com/kentarokamiyama/attendancemanagementapi/entitiy/Error.java
@@ -0,0 +1,18 @@
+package com.kentarokamiyama.attendancemanagementapi.entitiy;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Error {
+
+    Map<String,String> errorMap;
+
+}
